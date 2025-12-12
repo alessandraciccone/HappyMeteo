45a4f022a07576cc4df5cd77d048fb2bbad2efc6
alessandraciccone <alessandraciccone@icloud.com>
2025-12-12 10:50:50 +0100

    adds css Home, svg Icon

--- vite-project/src/component/Home.jsx
+++ vite-project/src/component/Home.jsx
@@ -3,6 +3,12 @@ import { useNavigate } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import "../css/Home.css";
+import SunIcon from "./SunIcon";
+import RainIcon from "./RainIcon";
+import Snow from "./Snow";
+import Rainbow from "./Rainbow";
+import Sfondo from "../assets/img/sfondo.png";
 const Home = () => {
   const [city, setCity] = useState("");
   const navigate = useNavigate();
@@ -14,27 +20,43 @@ const Home = () => {
   };
 
   return (
-    <div className="container">
-      <Card>
-        <Card.Body>
-          <Card.Title className="titleC"> Happy Meteo!</Card.Title>
-          <Card.Subtitle className="subC">
-            {" "}
-            Cerca la tua città per scoprire il consiglio del giorno
-          </Card.Subtitle>
-          <Form.Control
-            typoe="text"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            placeholder="Iserisci la tua città!"
-            className="inputC"
-          />
-          <Button className="btnC" onClick={handleSearch}>
-            {" "}
-            Vai!
-          </Button>
-        </Card.Body>
-      </Card>
+    <div>
+      <h1> Happy Meteo!</h1>
+
+      <div className="container">
+        <Card className="cardHome">
+          <Card.Body>
+            <div className="emoji-strip">
+              <SunIcon />
+              <RainIcon />
+              <Snow />
+              <Rainbow />
+            </div>
+
+            <Card.Subtitle className="subC">
+              {" "}
+              Cerca la tua città per scoprire il consiglio del giorno!
+            </Card.Subtitle>
+            <Form>
+              <Form.Control
+                type="text"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                placeholder="Iserisci la tua città!"
+                className="inputC"
+              />
+            </Form>
+            <Button className="btnC mx-auto d-block" onClick={handleSearch}>
+              {" "}
+              Vai!
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+
+      <div className="sfondi">
+        <img src={Sfondo} alt="sfondo" className="sImg"></img>
+      </div>
     </div>
   );
 };
---
+++ vite-project/src/component/Snow.jsx
@@ -0,0 +1,32 @@
+import "../css/SnowIcon.css";
+
+const Snow = () => {
+  return (
+    <svg
+      viewBox="-0.5 -0.5 16 16"
+      className="icon-snow"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      id="Snow--Streamline-Iconoir"
+      height="16"
+      width="16"
+    >
+      <desc>Snow Streamline Icon: https://streamlinehq.com</desc>
+      <path
+        d="m7.5 7.5 0 3.125m0 3.125 0 -3.125m0 0 -2.8125 -1.5625M7.5 10.625l2.8125 1.5625M7.5 10.625l2.8125 -1.5625M7.5 10.625l-2.8125 1.5625"
+        stroke="#000000"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1"
+      ></path>
+      <path
+        d="M12.5 11.004562499999999c0.9335625000000001 -0.36575 1.875 -1.199 1.875 -2.8795625000000005 0 -2.5 -2.0833125 -3.125 -3.125 -3.125 0 -1.25 0 -3.75 -3.75 -3.75S3.75 3.75 3.75 5c-1.0416687500000001 0 -3.125 0.625 -3.125 3.125 0 1.6805625 0.9414250000000001 2.5138125000000002 1.875 2.8795625000000005"
+        stroke="#000000"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1"
+      ></path>
+    </svg>
+  );
+};
+export default Snow;
